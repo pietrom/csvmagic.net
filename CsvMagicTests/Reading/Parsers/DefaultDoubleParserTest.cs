a8f291b21384839e842e412e87adf5761a174757
Pietro Martinelli <pietro.martinelli@codiceplastico.com>
2023-11-09 09:46:51 +0100

    Add test cases for DefaultDoubleRenderer and DefaultDoubleParser (#11)

---
+++ CsvMagicTests/Reading/Parsers/DefaultDoubleParserTest.cs
@@ -0,0 +1,33 @@
+using CsvMagic;
+using CsvMagic.Reading.Parsers;
+
+namespace CsvMagicTests.Reading.Parsers;
+
+[TestFixture]
+public class DefaultDoubleParserTest
+{
+    private readonly DefaultDoubleParser parser = new DefaultDoubleParser();
+
+    [TestCase("1234", 1234)]
+    [TestCase("1234.56", 1234.56)]
+    public void ParseUsingDefaults(string text, double value)
+    {
+        var parsed = parser.ParseNext(CsvReadingContextHelper.ContextFrom(CsvOptions.Default()), text);
+        Assert.That(parsed.Item1, Is.EqualTo(value));
+    }
+
+    [TestCase("1234", 1234)]
+    [TestCase("1234,56", 1234.56)]
+    public void ParseCustom(string text, double value)
+    {
+        var parsed = parser.ParseNext(CsvReadingContextHelper.ContextFrom(new CsvOptions(';', '"', ',', false)), text);
+        Assert.That(parsed.Item1, Is.EqualTo(value));
+    }
+
+    [TestCase("\"1234,56\"", 1234.56)]
+    public void ParseCustomWithQuotingNeeded(string text, double value)
+    {
+        var parsed = parser.ParseNext(CsvReadingContextHelper.ContextFrom(new CsvOptions(',', '"', ',', false)), text);
+        Assert.That(parsed.Item1, Is.EqualTo(value));
+    }
+}
---
+++ CsvMagicTests/Writing/Renderers/DefaultDoubleRendererTest.cs
@@ -0,0 +1,36 @@
+using CsvMagic;
+using CsvMagic.Writing.Renderers;
+
+namespace CsvMagicTests.Writing.Renderers;
+
+[TestFixture]
+public class DefaultDoubleRendererTest
+{
+    private readonly DefaultDoubleRenderer renderer = new DefaultDoubleRenderer();
+
+    [TestCase(null, "")]
+    [TestCase(1234, "1234")]
+    [TestCase(1234.56, "1234.56")]
+    public void RenderUsingDefaulta(double? input, string output)
+    {
+        var result = renderer.RenderObject(CsvWritingContextHelper.ContextFrom(CsvOptions.Default()), input);
+        Assert.That(result, Is.EqualTo(output));
+    }
+
+    [TestCase(null, "")]
+    [TestCase(1234, "1234")]
+    [TestCase(1234.56, "1234,56")]
+    public void RenderCustom(double? input, string output)
+    {
+        var result = renderer.RenderObject(CsvWritingContextHelper.ContextFrom(new CsvOptions(';', '"', ',', false)), input);
+        Assert.That(result, Is.EqualTo(output));
+    }
+
+
+    [TestCase(1234.56, "\"1234,56\"")]
+    public void RenderCustomWithQuotingNeeded(double? input, string output)
+    {
+        var result = renderer.RenderObject(CsvWritingContextHelper.ContextFrom(new CsvOptions(',', '"', ',', false)), input);
+        Assert.That(result, Is.EqualTo(output));
+    }
+}
