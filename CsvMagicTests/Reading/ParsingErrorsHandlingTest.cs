c1249b112dc4df2a95b45dad260a3d4cd3a85ad0
Pietro Martinelli <pietro.martinelli@codiceplastico.com>
2023-11-09 11:59:04 +0100

    Add tets case for errors handling in multi-level DTO parsing

--- CsvMagicTests/Reading/ParsingErrorsHandlingTest.cs
+++ CsvMagicTests/Reading/ParsingErrorsHandlingTest.cs
@@ -1,13 +1,12 @@
 using CsvMagic;
 using CsvMagic.Reading;
 using CsvMagic.Reading.Parsers;
+using CsvMagicTests.DataTypes;
 
 namespace CsvMagicTests.Reading;
 
 public class ParsingErrorsHandlingTest
 {
-    private readonly CsvReadingEngine<Row> engine = new CsvReadingEngineFactory().Create<Row>();
-
     class Row
     {
         public string Text { get; set; }
@@ -17,7 +16,7 @@ public class ParsingErrorsHandlingTest
     [Test]
     public Task ReadWithoutErrors()
     {
-        return Read(@"Text,Value
+        return Read<Row>(@"Text,Value
 A,1
 B,2,C,33333
 ");
@@ -26,7 +25,7 @@ B,2,C,33333
     [Test]
     public void ShouldTrackErrorLineNumber()
     {
-        var ex = Assert.ThrowsAsync<CsvReadingException>(() => Read(@"Text,Value
+        var ex = Assert.ThrowsAsync<CsvReadingException>(() => Read<Row>(@"Text,Value
 A,1
 B,2,
 C,33xx333
@@ -38,7 +37,7 @@ D,8765
     [Test]
     public void ShouldTrackErrorLineText()
     {
-        var ex = Assert.ThrowsAsync<CsvReadingException>(() => Read(@"Text,Value
+        var ex = Assert.ThrowsAsync<CsvReadingException>(() => Read<Row>(@"Text,Value
 A,1
 B,2,
 C,33xx333
@@ -50,7 +49,7 @@ D,8765
     [Test]
     public void ShouldTrackTokenText()
     {
-        var ex = Assert.ThrowsAsync<CsvReadingException>(() => Read(@"Text,Value
+        var ex = Assert.ThrowsAsync<CsvReadingException>(() => Read<Row>(@"Text,Value
 A,1
 B,2,
 C,33xx333
@@ -62,7 +61,7 @@ D,8765
     [Test]
     public void ShouldTrackParserTag()
     {
-        var ex = Assert.ThrowsAsync<CsvReadingException>(() => Read(@"Text,Value
+        var ex = Assert.ThrowsAsync<CsvReadingException>(() => Read<Row>(@"Text,Value
 A,1
 B,2,
 C,33xx333
@@ -71,11 +70,23 @@ D,8765
         Assert.That(ex.ParserTag, Is.EqualTo(nameof(DefaultIntParser)));
     }
 
-    private Task<IEnumerable<Row>> Read(string input)
+    [Test]
+    public void ShouldTrackErrorsProperlyInMultilevelParsing()
     {
-        return engine.Read(input, CsvOptions.Builder().WithHeaders().Build());
+        var ex = Assert.ThrowsAsync<CsvReadingException>(() => Read<Level0>(@"Field00,Field10,Field20,Field21,Field22,Field12,Field02
+F00,11,Aaa,1978-03-19,Bbb,1978-11-11,19
+G00,22,Ccc,2100-06-07,Ddd,2211-12-12,17
+H00,22,Ccc,2100:06:07,Ddd,2211-12-12,17
+I00,22,Ccc,2100-06-07,Ddd,2211-12-12,17
+"));
+        Assert.That(ex.LineNumber, Is.EqualTo(3));
+        Assert.That(ex.LineText, Is.EqualTo("H00,22,Ccc,2100:06:07,Ddd,2211-12-12,17"));
+        Assert.That(ex.TokenText, Is.EqualTo("2100:06:07"));
+        Assert.That(ex.ParserTag, Is.EqualTo(nameof(DefaultDateOnlyParser)));
     }
 
-
-    
+    private Task<IEnumerable<TRow>> Read<TRow>(string input) where TRow : new()
+    {
+        return new CsvReadingEngineFactory().Create<TRow>().Read(input, CsvOptions.Builder().WithHeaders().Build());
+    }
 }
