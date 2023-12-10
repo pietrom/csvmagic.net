c8b910f460ca5a31681ff9e024de31911eef85d5
Pietro Martinelli <amicofragile@gmail.com>
2023-12-10 20:13:49 +0100

    Provide unit tests for DictionaryExtension

---
+++ CsvMagicTests/Helpers/DictionaryExtensionTest.cs
@@ -0,0 +1,30 @@
+﻿using CsvMagic.Helpers;
+
+namespace CsvMagicTests.Helpers;
+
+[TestFixture]
+public class DictionaryExtensionTest {
+    private static readonly IDictionary<string, string> Dictionary = new Dictionary<string, string> {
+        { "foo", "bar" }
+    };
+
+    [Test]
+    public void GetExistingKeyProvidingDefaultValue() {
+        Assert.That(Dictionary.GetOrDefault("foo", "default-foo"), Is.EqualTo("bar"));
+    }
+
+    [Test]
+    public void GetNotExistingKeyProvidingDefaultValue() {
+        Assert.That(Dictionary.GetOrDefault("other-key", "default-foo"), Is.EqualTo("default-foo"));
+    }
+
+    [Test]
+    public void GetExistingKeyProvidingDefaultValueGenerator() {
+        Assert.That(Dictionary.GetOrDefault("foo", () => "default-foo"), Is.EqualTo("bar"));
+    }
+
+    [Test]
+    public void GetNotExistingKeyProvidingDefaultValueGenerator() {
+        Assert.That(Dictionary.GetOrDefault("other-key", () => "default-foo"), Is.EqualTo("default-foo"));
+    }
+}
