864a126094bb9dc8ffb9ee1504995ac7923e4f07
Pietro Martinelli <amicofragile@gmail.com>
2023-12-10 21:39:02 +0100

    Remove global guard against null values: leave the choice to subclasses

--- CsvMagic/Writing/Renderers/QuotableFieldRenderer.cs
+++ CsvMagic/Writing/Renderers/QuotableFieldRenderer.cs
@@ -4,10 +4,6 @@ namespace CsvMagic.Writing.Renderers;
 
 public abstract class QuotableFieldRenderer<T> : FieldRenderer {
     public string RenderObject(CsvWritingContext context, object? value) {
-        if (value == null) {
-            return string.Empty;
-        }
-
         var text = RenderValue(context, (T)value);
 
         var escaped = text.Replace($"{context.Options.Quoting}", $"{context.Options.Quoting}{context.Options.Quoting}");
