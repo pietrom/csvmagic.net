6e7959e39e3e21c69f496acae4a33e9a2efa39cc
Pietro Martinelli <amicofragile@gmail.com>
2023-12-19 08:42:11 +0100

    Add one more test case

--- CsvMagicTests/Writing/CsvWritingEngineTest.cs
+++ CsvMagicTests/Writing/CsvWritingEngineTest.cs
@@ -29,7 +29,7 @@ public class CsvWritingEngineTest {
     }
 
     [Test]
-    public async Task SerializeCsvWithCustomLabels() {
+    public async Task SerializeCsvWithCustomLabelsThroughAttribute() {
         var engine = new CsvWritingEngineFactory()
                 .RegisterRenderer<DateOnly>(new DateOnlyRenderer("yyyyMMdd"))
                 .Create<CsvWriteDataWithCustomLabels>()
@@ -46,6 +46,31 @@ public class CsvWritingEngineTest {
         Assert.That(result, Is.EqualTo(@"Counter;TextValue;LongValue;BirthDay;Another Day;Another String
 1;pietrom;19;1978-03-19;20080522;""pietro;m""
 2;cristinar;11;1978-11-11;20080522;""cristina;r""
+"));
+    }
+
+    [Test]
+    public async Task SerializeCsvWithCustomLabelsThroughExplicitConfiguration() {
+        var engine = new CsvWritingEngineFactory()
+                .Create<CsvWriteData>()
+            ;
+
+        engine.Configure(x => x.BirthDay).UsingLabel("The day of your birth");
+        engine.Configure(x => x.StringValue).UsingLabel("TextValue");
+        engine.Configure(x => x.OtherDay).UsingLabel("Another Day");
+        engine.Configure(x => x.OtherString).UsingLabel("Another String");
+
+        var stream = new MemoryStream();
+        await engine.Write(Options, new[]
+        {
+            new CsvWriteData { Counter = 1, LongValue = 19, StringValue = "pietrom", BirthDay = new DateOnly(1978, 3, 19), OtherDay = new DateOnly(2008, 5, 22), OtherString = "pietro;m"},
+            new CsvWriteData { Counter = 2, LongValue = 11, StringValue = "cristinar", BirthDay = new DateOnly(1978, 11, 11), OtherDay = new DateOnly(2008, 5, 22), OtherString = "cristina;r"},
+        }, new StreamWriter(stream));
+        stream.Seek(0, SeekOrigin.Begin);
+        var result = Encoding.UTF8.GetString(stream.ToArray());
+        Assert.That(result, Is.EqualTo(@"Counter;TextValue;LongValue;The day of your birth;Another Day;Another String
+1;pietrom;19;1978-03-19;2008-05-22;""pietro;m""
+2;cristinar;11;1978-11-11;2008-05-22;""cristina;r""
 "));
     }
 
