a78833df62053ffa7dccd2b0dd4ba94ba389d2de
Pietro Martinelli <amicofragile@gmail.com>
2023-12-20 10:11:55 +0100

    Add one more test case, in order to cover an additional branch

---
+++ CsvMagicTests/Writing/Renderers/DefaultStringRendererTest.cs
@@ -0,0 +1,19 @@
+﻿using CsvMagic;
+using CsvMagic.Writing.Renderers;
+using static CsvMagicTests.Writing.CsvWritingContextHelper;
+
+namespace CsvMagicTests.Writing.Renderers;
+
+[TestFixture]
+public class DefaultStringRendererTest {
+    private readonly DefaultStringRenderer renderer = new ();
+
+    [TestCase(null, "")]
+    [TestCase("", "")]
+    [TestCase("abcd", "abcd")]
+    [TestCase("ab,cd", "\"ab,cd\"")]
+    public void RenderUsingDefaults(string? input, string output) {
+        var result = renderer.RenderObject(ContextFrom(CsvOptions.Default()), input);
+        Assert.That(result, Is.EqualTo(output));
+    }
+}
