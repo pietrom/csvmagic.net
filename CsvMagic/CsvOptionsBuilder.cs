09eec893e82dbbf8a9ab83b0f773446d0ce0b408
Pietro Martinelli <amicofragile@gmail.com>
2023-12-26 21:30:15 +0100

    One type per file. Remove unused, legacy method

---
+++ CsvMagic/CsvOptionsBuilder.cs
@@ -0,0 +1,41 @@
+﻿namespace CsvMagic;
+
+public class CsvOptionsBuilder {
+    private char delimiter = ',';
+    private char quoting = '"';
+    private char decimalSeparator = '.';
+    private bool handleHeaderRow = true;
+    private bool fullyQualifyNestedProperties = false;
+
+    public CsvOptionsBuilder WithDelimiter(char delimiter) {
+        this.delimiter = delimiter;
+        return this;
+    }
+
+    public CsvOptionsBuilder WithQuoting(char quoting) {
+        this.quoting = quoting;
+        return this;
+    }
+
+    public CsvOptionsBuilder WithDecimalSeparator(char decimalSeparator) {
+        this.decimalSeparator = decimalSeparator;
+        return this;
+    }
+
+    public CsvOptionsBuilder WithoutHeaders() {
+        handleHeaderRow = false;
+        return this;
+    }
+
+    public CsvOptionsBuilder WithHeaders() {
+        handleHeaderRow = true;
+        return this;
+    }
+
+    public CsvOptionsBuilder FullyQualifyNestedProperties() {
+        fullyQualifyNestedProperties = true;
+        return this;
+    }
+
+    public CsvOptions Build() => new SimpleCsvOptions(delimiter, quoting, decimalSeparator, handleHeaderRow, fullyQualifyNestedProperties);
+}
---
+++ CsvMagic/SimpleCsvOptions.cs
@@ -0,0 +1,3 @@
+﻿namespace CsvMagic;
+
+internal record SimpleCsvOptions(char Delimiter, char Quoting, char DecimalSeparator, bool HandleHeaderRow, bool FullyQualifyNestedProperties) : CsvOptions;
